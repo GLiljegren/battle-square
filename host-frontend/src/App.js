dfdb5e230fb1323d5241cbf3668899eeaa72c6b7
Gustaf Liljegren <liljegren.gustaf@gmail.com>
2020-05-15 22:15:49 +0200

    React-setup for host-frontend

---
+++ host-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+	<h1>BattleSquare</h1>
+	<button>Create New Game!</button>
+    </div>
+  );
+}
+
+export default App;

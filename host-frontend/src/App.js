f365c7e87415c05365b8dc1af2f6a7450bad28b7
GLiljegren <liljegren.gustaf@gmail.com>
2020-05-15 22:56:04 +0200

    connect backend

--- host-frontend/src/App.js
+++ host-frontend/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 
 const App = () => {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", (data) => {
+      setResponse(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-	<h1>BattleSquare</h1>
-	<button>Create New Game!</button>
+      <h1>BattleSquare</h1>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+      <button>Create New Game!</button>
     </div>
   );
-}
+};
 
 export default App;

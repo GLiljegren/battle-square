a9892edfcc2e5c4ed49e456ef3ce545a5a2eca9f
GLiljegren <liljegren.gustaf@gmail.com>
2020-05-17 17:02:23 +0200

    change cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,27 @@ const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
 const app = express();
-app.use(cors());
+
+var allowedOrigins = [
+  "http://localhost:3000",
+  "https://stupefied-hawking-a5c42a.netlify.app",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(index);
 
 const server = http.createServer(app);

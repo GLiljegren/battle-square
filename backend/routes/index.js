78d8029c9d33db20b87cbfb982cee9e1ec3122c9
GLiljegren <liljegren.gustaf@gmail.com>
2020-05-17 10:39:46 +0200

    add create route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
 
+router.get("/create", (req, res) => {
+  res.send({ response: { data: "45HR" } }).status(200);
+});
+
 module.exports = router;
--- host-frontend/src/App.js
+++ host-frontend/src/App.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:4001";
+//import axios from "axios";
+const ENDPOINT = "http://localhost:4001/";
 
 const App = () => {
-  const [response, setResponse] = useState("");
+  const [id, setId] = useState(null);
 
-  useEffect(() => {
+  useEffect(() => {}, []);
+
+  const createGameHandler = () => {
     const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", (data) => {
-      setResponse(data);
+    socket.on("create", (_id) => {
+      setId(_id);
     });
-  }, []);
+  };
 
   return (
     <div className="App">
       <h1>BattleSquare</h1>
-      <p>
-        It's <time dateTime={response}>{response}</time>
-      </p>
-      <button>Create New Game!</button>
+      {id ? <h2>{id}</h2> : <h2>no id</h2>}
+      <button onClick={createGameHandler}>Create New Game!</button>
+      <input></input>
     </div>
   );
 };
